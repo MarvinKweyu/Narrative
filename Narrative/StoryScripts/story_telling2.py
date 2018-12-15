706215bc83d1c6ff0bd959f28aaaec28bbf8a84a
MarvinKweyu <mkweyu1@gmail.com>
2018-12-15 18:22:23 +0300

    Organized stories according to genre and grouped files

--- Narrative/StoryScripts/story_telling2.py
+++ Narrative/StoryScripts/story_telling2.py
@@ -2,11 +2,13 @@
 
 #takes user input,changes content in an existing file
 # to match input and makes a new file of it saving it as a pdf and displaying it with
-# an option to send to the otehr game participants
+# an option to send to the other game participants
 
 import PyPDF2
 import fileinput
 import subprocess
+import os
+import sys
 
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
@@ -16,27 +18,29 @@ from PIL import Image
 class Application(object):
     'This is for a story'
 
-    def __init__(self,play_file,name):
+    def __init__(self,play_file,main_character,character1,character2):
         self.play_file = play_file
-        self.name = name
+        self.main_character = main_character
+        self.character1 = character1
+        self.character2 = character2
         self.get_information()
 
     def get_information(self):
         '''Open the pdf file ,edit and write edited work to a new file
         Has two text files; intermediate_txt(has words extracted) and final_text(has words changed)'''
 
-        self.replacement_words = {'Sakura':self.name,
-                                 'SAKURA':self.name.upper(),
-                                 'Melvin':'Emmanuel'
+        self.replacement_words = {'Alaina':self.main_character,
+                                 'Darcy':self.character1,
+                                 'Conrad':self.character2
                             }
 
         self.file =  PyPDF2.PdfFileReader(open(self.play_file,'rb'))
         self.pdf_writer = PyPDF2.PdfFileWriter()#create blank pdf
 
         print("Creating intermediate text...")
-        self.intermediate_txt = open('intermediate.txt','a')
-        print("Extracting text...")
+        self.intermediate_txt = open('../Programfiles/Intermediates/intermediate.txt','a')
 
+        print("Extracting text...")
         for current_page in range(1,self.file.numPages):
             self.page_obj = self.file.getPage(current_page)
             self.content = self.page_obj.extractText() #file name
@@ -48,10 +52,10 @@ class Application(object):
 
         #open text file to write into and append content
         print("Creating new file...")
-        self.new_output_file = open('final_text.txt','w')
+        self.new_output_file = open('../Programfiles/Finaltexts/final_text.txt','w')
         # takes file name as self.content
         print("Adding new content...")
-        for line in fileinput.input('intermediate.txt',inplace=False):
+        for line in fileinput.input('../Programfiles/Intermediates/intermediate.txt',inplace=False):
             line=line.rstrip()
             if not line:
                 continue
@@ -65,6 +69,7 @@ class Application(object):
 
     def display(self):
         'Show the information to user with default application on PC'
+
         print("Opening file...")
         subprocess.Popen(['dolphin','OurGameStory.pdf'])
 
@@ -73,12 +78,12 @@ class Application(object):
         'take the final text and convert to pdf'
 
         print("Generating PDF...")
-        self.text_file = open("final_text.txt", "r")  # text file I need to convert
+        self.text_file = open("../Programfiles/Finaltexts/final_text.txt", "r")  # text file I need to convert
         self.lines = self.text_file.readlines()
         self.text_file.close()
         self.i = 750
         self.num_of_lines = 0
-        self.my_pdf = canvas.Canvas('OurGameStory.pdf') #the pdf created
+        self.my_pdf = canvas.Canvas('../UserPdfFiles/OurGameStory.pdf') #the pdf created
 
         while self.num_of_lines < len(self.lines):
             if self.num_of_lines - len(self.lines) < 65: # I'm gonna write every 65 lines because I need it like that
@@ -98,13 +103,38 @@ class Application(object):
         print("Saving...")
         self.my_pdf.save()
         print("Done...")
-        self.display()
+        # self.display()
 
 
 #main
 def main():
-    internal_file = 'Romance/A_Secret_Kiss.pdf'
-    name = input("Enter your name: ").lower().capitalize()
-    new_user = Application(internal_file,name)
+
+    available_genre = os.listdir('../Genre/')
+
+    for item in available_genre:
+        print(item)
+    print("\n")
+
+    # genre = input("\nPlease select a genre from the availables: ").lower().capitalize()
+    # if genre =='Adventure':
+    #     internal_file = '../Genre/Adventure/adventure.pdf'
+    # elif genre == 'Romance':
+    #     internal_file = '../Genre/Romance/romance.pdf'
+    # elif genre == 'Thriller':
+    #     internal_file = '../Genre/Thriller/thriller.pdf'
+    # else:
+    #     print("There is no category",genre)
+    #     option = input("Reload? (Y/N): ").upper()
+    #     if option == 'Y' or option == 'YES':
+    #         main()
+    #     elif option == 'N' or option =='NO':
+    #         print("Goodbye")
+    #         sys.exit()
+    internal_file = '../Programfiles/Testers/Story.pdf'
+    main_character = input("Enter your name: ").lower().capitalize()
+    character1 = input("Enter second character name: ").lower().capitalize()
+    character2 = input("Enter third character name: ").lower().capitalize()
+    print("\n")
+    new_user = Application(internal_file,main_character,character1,character2)
     return
 main()
